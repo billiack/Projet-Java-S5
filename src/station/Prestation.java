3bae75348405f373480ffa3bdf833566be2242ac
billiack <150272222+billiack@users.noreply.github.com>
2026-01-08 16:48:37 +0100

    Add abstract methods

--- src/station/Prestation.java
+++ src/station/Prestation.java
@@ -2,4 +2,9 @@ package station;
 
 public abstract class Prestation {
     private String categorie;
+
+    public abstract double lavage();
+    public abstract double sechage();
+    public abstract double prelavage();
+    public abstract double nettoyage();
 }

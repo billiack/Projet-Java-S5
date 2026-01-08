4f340f49d2ae3065cd8e7da5bf389785915426b7
neilpernon2004-svg <neilpernon2004@gmail.com>
2026-01-08 22:26:35 +0100

    Add price attribute to RendezVous class
    Added a price attribute and updated constructor and toString method.

--- src/station/RendezVous.java
+++ src/station/RendezVous.java
@@ -1,15 +1,34 @@
-/*
- * Projet Java
- *
- * Créé par :
- * PRAK Billy
- * PERNON Neil
- * SANTOS Emmanuel
- */
-
 package station;
 
 public class RendezVous {
     private Client client;
     private Prestation prestation;
+    private double prix;
+
+    public RendezVous(Client client, Prestation prestation) {
+        this.client = client;
+        this.prestation = prestation;
+        this.prix = prestation.nettoyage();
+    }
+
+    @Override
+    public String toString() {
+        return "RendezVous{" +
+               "client=" + client +
+               ", prestation=" + prestation +
+               ", prix=" + prix +
+               '}';
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public Prestation getPrestation() {
+        return prestation;
+    }
+
+    public double getPrix() {
+        return prix;
+    }
 }

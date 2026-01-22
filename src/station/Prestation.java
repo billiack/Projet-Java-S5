eaac44266f7ede5ad25baa73b44d64378486c4c0
neilpernon2004-svg <neilpernon2004@gmail.com>
2026-01-22 16:16:21 +0100

    Update Prestation.java

--- src/station/Prestation.java
+++ src/station/Prestation.java
@@ -10,19 +10,24 @@
 package station;
 
 public abstract class Prestation {
+    // Classe mère des prestations : définit le "contrat" commun à toutes les prestations
+
     protected String categorie;
+    // Catégorie du véhicule (A, B ou C)
 
     public Prestation(String categorie) {
+        // Initialisation de la catégorie commune aux prestations
         this.categorie = categorie;
     }
 
     public String getCategorie() {
+        // Permet de récupérer la catégorie du véhicule
         return categorie;
     }
 
+    // Méthodes à implémenter dans les classes filles (Express / Sale / TrèsSale)
     public abstract double lavage();
     public abstract double sechage();
     public abstract double prelavage();
     public abstract double nettoyage();
 }
-
